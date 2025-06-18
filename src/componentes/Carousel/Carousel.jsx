ba8194ef939ebd1d4293d26c7ba84c7f2c9e29da
ignaarguello <igna.arguello2110@hotmail.com>
2025-06-18 13:51:39 -0300

    Add/Mas productos

--- src/componentes/Carousel/Carousel.jsx
+++ src/componentes/Carousel/Carousel.jsx
@@ -1,49 +1,54 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 //Estilos
-import './Carousel.css';
+import "./Carousel.css";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-fade';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 
 // import required modules
-import { Pagination, Autoplay, EffectFade } from 'swiper/modules';
+import { Pagination, Autoplay, EffectFade } from "swiper/modules";
 
 //Imagenes
-import Petshop1 from '/imagenes/carousel/petshop1.png';
-import Correas from '/imagenes/carousel/correas.png';
-import OldPrince from '/imagenes/carousel/old_prince.png';
-import Eukanuba from '/imagenes/carousel/eukanuba_banner.png';
+import Petshop1 from "/imagenes/carousel/petshop1.png";
+import OldPrince from "/imagenes/carousel/old_prince.png";
+import Eukanuba from "/imagenes/carousel/eukanuba_banner.png";
 
 // Array de imagenes para carousel
-const ImagenesCarousel = [Petshop1, Eukanuba, Correas, OldPrince];
+const ImagenesCarousel = [Petshop1, Eukanuba, OldPrince];
 
 export default function Carousel() {
-    return (
-        <div id='ContenedorGeneralCarousel'>
-            <Swiper
-                navigation={true}
-                modules={[Pagination, Autoplay, EffectFade]}
-                className="Swiper"
-                direction={'horizontal'}
-                pagination={{
-                    clickable: true,
-                }}
-                autoplay={{
-                    delay: 3500,
-                    disableOnInteraction: false,
-                }}
-                loop={true}
-                effect={'fade'}
-            >
-                {ImagenesCarousel.map((slide) => (
-                    <SwiperSlide key={slide.id} style={{ backgroundImage: `url(${slide})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    )
+  return (
+    <div id="ContenedorGeneralCarousel">
+      <Swiper
+        navigation={true}
+        modules={[Pagination, Autoplay, EffectFade]}
+        className="Swiper"
+        direction={"horizontal"}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        effect={"fade"}
+      >
+        {ImagenesCarousel.map((slide) => (
+          <SwiperSlide
+            key={slide.id}
+            style={{
+              backgroundImage: `url(${slide})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
